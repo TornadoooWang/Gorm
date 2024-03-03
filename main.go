b1296101e4d9ea7fbe4b3565fc92cc30c4a93a93
TornadoooWang <1642127610@qq.com>
2024-03-04 01:49:23 +0800

    feat: add db conn

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
 func main() {
-	// test
+	dsn := "root:682648@tcp(localhost)/test_2024?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		print(err)
+	}
+
 }
